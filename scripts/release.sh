1f3a873b85ab3af16d0ce2f390dee5dd383c2d03
Joshua Miles <joshmilesproduction@gmail.com>
2025-07-01 16:14:00 +0100

    changes

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,6 +13,15 @@ echo "Bumping version with '$VERSION_BUMP'..."
 # Update the version, create a commit, and tag it
 npm version "$VERSION_BUMP" -m "chore(release): %s"
 
+# Get the new version from package.json
+NEW_VERSION=$(node -p "require('./package.json').version")
+
+# Update the version in conveyor.conf
+sed -i.bak -E "s/(^\s*version:\s*).*/\1$NEW_VERSION/" conveyor.conf
+rm -f conveyor.conf.bak
+
+echo "Updated conveyor.conf version to $NEW_VERSION"
+
 echo "Pushing commit and tags..."
 
 # Push the commit and the new tag to the remote repository

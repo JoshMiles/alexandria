ffaf1837280cb46b0aaf3caa13664d8d5d24938f
Joshua Miles <joshmilesproduction@gmail.com>
2025-07-18 17:52:00 +0100

    Add __dirname and __filename to make and test scripts
    Imported fileURLToPath from 'url' and defined __filename and __dirname in both make.mjs and test.mjs to enable usage of current file and directory paths.

--- scripts/make.mjs
+++ scripts/make.mjs
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 import { spawnSync } from 'child_process';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log('Running make script...');
 
